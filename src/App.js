50cc367ca836d99d45248a85135a760861577722
nidhip24 <nidhipkathiriya@gmail.com>
2025-02-23 14:33:54 -0500

    chore: removing unused refrences

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 
 function App() {
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import TodoForm from './TodoForm';
 
 function TodoList({ todos, onUpdate, onDelete, onToggle, onStartEdit, editingId, editingText, onEditChange }) {
   return (
